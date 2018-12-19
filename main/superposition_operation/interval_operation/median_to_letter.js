fd81d26d48a8383a2068fd428484358d1b21628b
Harper324 <1060010408@qq.com>
2018-12-19 14:59:56 +0800

    correct median_to_letter

--- main/superposition_operation/interval_operation/median_to_letter.js
+++ main/superposition_operation/interval_operation/median_to_letter.js
@@ -1,13 +1,21 @@
 'use strict';
 
 function median_to_letter(collection) {
-  return String.fromCharCode(96 + parseInt(get_median(collection)));
+  var result = get_median(collection);
+  if (result <= 26) {
+    result = String.fromCharCode(96 + parseInt(result));
+  } else {
+    var firstCode = parseInt(Math.floor((result - 1) / 26)) + 96;
+    var secondCode = parseInt((result - 1) % 26) + 97;
+    result = String.fromCharCode(firstCode) + String.fromCharCode(secondCode);
+  }
+  return result;
 }
 function get_median(element) {
   var lowMiddle = Math.floor((element.length - 1) / 2);
   var highMiddle = Math.ceil((element.length - 1) / 2);
 
-  return Math.floor((Number(array[lowMiddle]) + Number(array[highMiddle])) / 2);
+  return Math.ceil((Number(element[lowMiddle]) + Number(element[highMiddle])) / 2);
 }
 
 module.exports = median_to_letter;
