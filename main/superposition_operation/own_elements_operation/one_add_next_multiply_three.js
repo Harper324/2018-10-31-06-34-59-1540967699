939a66513a0b40981394dc26dc5240418ead550c
Harper324 <1060010408@qq.com>
2018-12-19 15:09:06 +0800

    one_add_next_multiply_three

--- main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ main/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,11 +1,9 @@
 'use strict';
 function one_add_next_multiply_three(collection) {
   var result = [];
-  for (var i = 1; i < collection.length - 1; i++) {
+  for (var i = 0; i < collection.length - 1; i++) {
     result.push(3 * (collection[i] + collection[i + 1]));
   }
   return result;
 }
-
-
 module.exports = one_add_next_multiply_three;
