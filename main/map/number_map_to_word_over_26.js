c084d64634c43ce8d926de155f7b4730f162b863
Harper324 <1060010408@qq.com>
2018-12-19 11:26:28 +0800

    correct number_map_to_word_over_26

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,6 @@
 'use strict';
 var number_map_to_word_over_26 = function (collection) {
-  return collections.map(function (element) {
+  return collection.map(function (element) {
     if (element <= 26) {
       element = String.fromCharCode(96 + parseInt(element));
     } else {
