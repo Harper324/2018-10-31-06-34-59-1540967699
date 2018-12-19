2b0ae70130ab0482645c47f50300a7fa03ca64de
Harper324 <1060010408@qq.com>
2018-12-19 17:14:02 +0800

    correct even_group_calculate_average

--- main/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -2,6 +2,7 @@
 var even_group_calculate_average = function (collection) {
     var evenIndex = get_even_index(collection);
     var evenNumber = get_even_number(evenIndex);
+
     var oneNumber = evenNumber.filter(function (element) {
         return element < 10;
     });
@@ -12,31 +13,35 @@ var even_group_calculate_average = function (collection) {
         return 99 < element && element < 1000;
     });
     var result = [];
-    if (oneNumber && twoNumber && threeNumber) {
+    if (!(oneNumber == false) && !(twoNumber == false) && !(threeNumber == false)) {
         result = [add(oneNumber) / oneNumber.length, add(twoNumber) / twoNumber.length, add(threeNumber) / threeNumber.length];
-    } else if (!oneNumber && !twoNumber && threeNumber) {
+    } else if (oneNumber == false && twoNumber == false && !(threeNumber == false)) {
         result = [add(threeNumber) / threeNumber.length];
-    } else {
+    } else if (oneNumber == false && twoNumber == false && threeNumber == false) {
         result = [0];
     }
+
     return result;
+
+
 
 };
 function get_even_index(element) {
-    return element.filter(function (value, index) {
-        if (index % 2 === 1) {
-            return value;
-        }
+    var evenIndex = element.filter(function (ele, index, element) {
+        return index % 2 === 1;
     });
+    return evenIndex;
 }
 function get_even_number(element) {
-    return element.filter(function (element) {
+    var evenNumber = element.filter(function (element) {
         return element % 2 === 0;
     });
+    return evenNumber;
 }
 function add(element) {
-    return element.reduce(function (a, b) {
+    var add = element.reduce(function (a, b) {
         return a + b;
-    });
+    }, 0);
+    return add;
 }
 module.exports = even_group_calculate_average;
